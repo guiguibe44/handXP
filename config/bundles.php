f8cad8e74b8f0b6ec7299844b27e007f9d4fdb07
guiguibe44 <guillaume.bex44@gmail.com>
2020-03-06 17:25:25 +0100

    sonata CKEditor

--- config/bundles.php
+++ config/bundles.php
@@ -25,4 +25,7 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Sonata\MediaBundle\SonataMediaBundle::class => ['all' => true],
     App\Application\Sonata\MediaBundle\ApplicationSonataMediaBundle::class => ['all' => true],
+    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
+    CoopTilleuls\Bundle\CKEditorSonataMediaBundle\CoopTilleulsCKEditorSonataMediaBundle::class => ['all' => true],
+
 ];
